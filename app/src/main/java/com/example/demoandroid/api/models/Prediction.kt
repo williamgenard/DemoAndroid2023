6cc21fe43330c803490109ff34431063b3a8aefb
williamgenard <139369021+williamgenard@users.noreply.github.com>
2023-09-08 13:29:33 +0200

    add serialized name annotation

--- app/src/main/java/com/example/demoandroid/api/models/Prediction.kt
+++ app/src/main/java/com/example/demoandroid/api/models/Prediction.kt
@@ -1,7 +1,10 @@
 package com.example.demoandroid.api.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Prediction(
     val age: Int,
     val count: Int,
+    @SerializedName("name")
     val name: String
 )
